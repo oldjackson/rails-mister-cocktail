cee7163cbca4bb0d052343e9e5281850e865e066
Lorenzo Fermi <lorenzofermi@gmail.com>
2018-02-24 00:43:08 +0000

    Add sorting of ingredients in dropdown and check on dose description

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,7 +1,6 @@
 class Cocktail < ApplicationRecord
   mount_uploader :photo, PhotoUploader
 
-
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, presence: true, uniqueness: true
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,4 +2,5 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
   validates :ingredient_id, uniqueness: {scope: :cocktail_id}
+  validates :description, presence: true
 end
